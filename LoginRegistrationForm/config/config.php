83f7f72ccdd22ce11d9d3accb83b16b67d59e0bc
Tauhidtonmoy <blackheartgunner@yahoo.com>
2017-09-09 14:11:12 +0600

    config.php and dbconfig.php modified

--- LoginRegistrationForm/config/config.php
+++ LoginRegistrationForm/config/config.php
@@ -1,6 +1,6 @@
     <?php
 	    define("DB_HOST", "localhost");
-	    define("DB_USER", "root");
-	    define("DB_PASS", "");
+	    define("DB_USER", "root"); #needs change on host
+	    define("DB_PASS", ""); 
 	    define("DB_NAME", "db_recruitment");
     ?>
