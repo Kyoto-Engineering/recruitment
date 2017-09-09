81dad84961c163d50d742a1f6bad37799d54ecb3
Arnab1500117 <arnab.r@keal.com.bd>
2017-09-09 16:40:09 +0600

    change file

--- LoginRegistrationForm/verify_email.php
+++ LoginRegistrationForm/verify_email.php
@@ -1,34 +1,39 @@
-<?php include_once "lib/Database.php"; ?>
 <?php
-	$db = new Database();
+
+ include_once 'dbconfig.php';
+
+$phone = $_GET['phone'];
+
+
+$count=0;
+	if($count == 0)
+	{
+		$sql="UPDATE tbl_user_reg SET status ='1' WHERE phone='$phone'";
+		mysql_query($sql);
+		?>
+		<script>
+		alert('successfully verified, please login to proceed further');
+        window.location.href='signup.php#tologin?success';
+        </script>
+		<?php
+ }
+
+
+
+	else
+	{
+		?>
+		<script>
+		alert('Sorry! email id already taken');
+		window.location.href='signup.php#tologin';
+        
+        </script>
+		<?php
+	}
+
+
 ?>
-	<?php
-		$phone = $_GET['phone'];
-	?>
-
-	<?php
-		$query = "UPDATE tbl_user_reg
-		 SET
-			status = '1'
-		 WHERE phone = '$phone' ";
-		 $update_row = $this->db->update($query);
-
-		 if ($update_row) { ?>
-		 	<script>
-					alert('successfully verified, please login to proceed further');
-			        window.location.href='LoginRegistrationForm/signup.php#tologin?success'; #make chage here.
-			        </script>
-		<?php } else { ?>
-				<script>
-					alert('Sorry! email id already taken');
-					window.location.href='LoginRegistrationForm/signup.php#toregister';
-			        
-			        </script> 
 
 
-	<?php } ?>
-
-
-		
-				
+	
 				
