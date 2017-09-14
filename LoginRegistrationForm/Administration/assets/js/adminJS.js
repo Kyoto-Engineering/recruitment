235f74260900fac25c5b8c0ba879de088ce4014d
Arnab1500117 <arnab.r@keal.com.bd>
2017-09-14 12:58:34 +0600

    relocated Administration folder

---
+++ LoginRegistrationForm/Administration/assets/js/adminJS.js
@@ -0,0 +1,3 @@
+$(document).load(function(){
+    $("div").addClass("intro");
+});
---
+++ LoginRegistrationForm/Administration/modules/specialization_decipline_creation.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        
+    </head>
+    
+    
+    <body>
+        
+        <div>
+            <form>
+                
+                <table>
+                    
+                    <caption> Specialization/Major/Concentration Creation </caption>
+                    
+                    <tr> <td> Discipline </td>    <td> <input type="text" name="discipline"> </td> </tr> 
+                    
+                    <tr>
+                        <td> Specialization/Major/Concentration </td> <td> <input type="text" name="jobTitle"> </td>
+                    </tr>
+                    
+                    <tr> <td id="formButton"> <input type="submit" value="Add"> </td> </tr>
+                </table>
+    
+            </form>
+        </div>
+        
+    </body>
+</html>
